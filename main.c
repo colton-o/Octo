96563cdc27c9549c1968743db49b100fe639a341
colton-smtafe <70926754+colton-smtafe@users.noreply.github.com>
2023-07-31 12:31:07 +0800

    added thickness, changing movement scheme

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
-#include "bezier.h"
+
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <math.h>
 
+#include "bezier.h"
+
 #define WINX 1000
 #define WINY 1000
-#define CURVE 50
-#define SDL_MAIN_HANDLED
+#define CURVE 100
+
 const int SCREEN_FPS = 100;
 const int SCREEN_TICK = 1000 / 60;
 
@@ -28,18 +31,20 @@ tentacle tentacles[8] = {0};
 SDL_Rect *octopus;
 
 void set_tentacle(tentacle *t) {
-  t->tentacle_pos.d.x = octopus->x + t->orig_pos.x;
-  t->tentacle_pos.d.y = octopus->y + t->orig_pos.y;
+  // t->tentacle_pos.d.x = octopus->x + t->orig_pos.x;
+  // t->tentacle_pos.d.y = octopus->y + t->orig_pos.y;
   t->tentacle_pos.c.x = (t->tentacle_pos.a.x + t->tentacle_pos.d.x) / 2;
-  t->tentacle_pos.c.y= (t->tentacle_pos.a.y + t->tentacle_pos.d.y) / 2;
+  t->tentacle_pos.c.y = (t->tentacle_pos.a.y + t->tentacle_pos.d.y) / 2;
   t->tentacle_pos.b.x = (t->tentacle_pos.c.x + t->tentacle_pos.d.x) / 2;
   t->tentacle_pos.b.y = (t->tentacle_pos.c.y + t->tentacle_pos.d.y) / 2;
 }
+
 void set_tentacle_distance(tentacle *t) {
   t->distance = sqrt(pow(t->tentacle_pos.d.x - (octopus->x), 2) +
                      pow(t->tentacle_pos.d.y - (octopus->y), 2));
 }
-int main(void) {
+
+int main(int argc, char *argv[]) {
   octopus = malloc(sizeof(SDL_Rect));
   octopus->h = 25;
   octopus->w = 25;
@@ -70,6 +75,11 @@ int main(void) {
   tentacles[7].orig_pos.x = -100;
   tentacles[7].orig_pos.y = -100;
 
+  for (int i = 0; i < 8; i++) {
+    tentacles[i].tentacle_pos.d.x = octopus->x + tentacles[i].orig_pos.x;
+    tentacles[i].tentacle_pos.d.y = octopus->y + tentacles[i].orig_pos.y;
+  }
+
   uint32_t framestart;
 
   int frametime;
@@ -87,49 +97,50 @@ int main(void) {
     framestart = SDL_GetTicks64();
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
-      case SDL_KEYDOWN:
-        switch (event.key.keysym.sym) {
-        case SDLK_w:
-          velocity.y = -1;
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym) {
+            case SDLK_w:
+              velocity.y = -1;
+              break;
+            case SDLK_s:
+              velocity.y = 1;
+              break;
+            case SDLK_a:
+              velocity.x = -1;
+              break;
+            case SDLK_d:
+              velocity.x = 1;
+              break;
+          }
           break;
-        case SDLK_s:
-          velocity.y = 1;
+        case SDL_KEYUP:
+          switch (event.key.keysym.sym) {
+            case SDLK_w:
+              velocity.y = 0;
+              break;
+            case SDLK_s:
+              velocity.y = 0;
+              break;
+            case SDLK_a:
+              velocity.x = 0;
+              break;
+            case SDLK_d:
+              velocity.x = 0;
+              break;
+          }
           break;
-        case SDLK_a:
-          velocity.x = -1;
-          break;
-        case SDLK_d:
-          velocity.x = 1;
-          break;
-        }
-        break;
-      case SDL_KEYUP:
-        switch (event.key.keysym.sym) {
-        case SDLK_w:
-          velocity.y = 0;
-          break;
-        case SDLK_s:
-          velocity.y = 0;
-          break;
-        case SDLK_a:
-          velocity.x = 0;
-          break;
-        case SDLK_d:
-          velocity.x = 0;
-          break;
-        }
-        break;
       }
-      if (event.type == SDL_QUIT)
-        return 0;
+      if (event.type == SDL_QUIT) return 0;
     }
 
-    octopus->x += velocity.x * speed;
-    octopus->y += velocity.y * speed;
+    // octopus->x += velocity.x * speed;
+    // octopus->y += velocity.y * speed;
+
+    tentacles[0].tentacle_pos.d.y += velocity.y * speed;
 
     SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 100);
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
     SDL_RenderFillRect(renderer, octopus);
 
     for (int i = 0; i < 8; i++) {
@@ -137,12 +148,25 @@ int main(void) {
       tentacles[i].tentacle_pos.a.y = octopus->y + (octopus->h / 2);
       set_tentacle_distance(&tentacles[i]);
 
-      if (tentacles[i].distance > 300 || tentacles[i].distance < 1)
-        set_tentacle(&tentacles[i]);
+      set_tentacle(&tentacles[i]);
+
+      SDL_Point curve_points_neg[CURVE] = {0};
+      SDL_Point curve_points_pos[CURVE] = {0};
 
       set_curve(tentacles[i].tentacle_pos, CURVE, curve_points);
 
+      for (int i = 0; i < CURVE; i++) {
+        int thick = round((double)i / (double)CURVE);
+        thick = (1 - thick);
+        curve_points_neg[i].x = curve_points[i].x - thick;
+        curve_points_neg[i].y = curve_points[i].y;
+        curve_points_pos[i].x = curve_points[i].x + thick;
+        curve_points_pos[i].y = curve_points[i].y;
+      }
+
       SDL_RenderDrawLines(renderer, curve_points, CURVE);
+      SDL_RenderDrawLines(renderer, curve_points_neg, CURVE);
+      SDL_RenderDrawLines(renderer, curve_points_pos, CURVE);
     }
 
     SDL_RenderPresent(renderer);
